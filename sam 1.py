dae1ab82e1098e4edb04eec93dce839fc65316b2
shwetal14 <95999701+shwetal14@users.noreply.github.com>
2021-12-30 00:18:32 +0530

    Add files via upload

---
+++ sam 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+def reversethestring(inputstring):
+    return inputstring[::-1]
+print(reversethestring("1234abcd"))
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
